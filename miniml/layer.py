832b64fd235741b6d14f065470f33e1075c25003
David Oniani <onianidavid@gmail.com>
2020-11-27 15:06:24 -0600

    Layers

--- miniml/layer.py
+++ miniml/layer.py
@@ -38,37 +38,20 @@ class Linear(Layer):
     """A linear layer."""
 
     def __init__(self, in_dim: int, out_dim: int, gpu: bool = False) -> None:
-        """Initilize variables.
-
-        Parameters
-        ----------
-        in_dim  : Number of input dimensions.
-        out_dim : Number of output dimensions.
-        gpu     : Specifies whether the computations are performed on GPU.
-        """
+        """Initilize variables."""
 
         self._weights: T.Tensor = T.Random.rand((in_dim, out_dim), gpu)
         self._biases: T.Tensor = T.Tensor.rand(out_dim, gpu)
         self._gpu: bool = gpu
 
     def forward(self, x: T.Tensor) -> T.Tensor:
-        """Perform a forward pass.
-        
-        Parameters
-        ----------
-        x : Data (activated or not) of the previous layer
-        """
+        """Perform a forward pass."""
 
         self._prev: T.Tensor = x
         return T.Tensor.dot(self._weights, x) + self._bias
 
     def backward(self, dF: T.Tensor) -> T.Tensor:
-        """Perform a backpropagation.
-
-        Parameters
-        ----------
-        dF: upper layer gradient
-        """
+        """Perform a backpropagation."""
 
         # Derivative of the cost function w.r.t. W
         self._dW: T.Tensor = T.Tensor.dot(dF, self._prev.T)
@@ -84,94 +67,3 @@ class Linear(Layer):
 
         self._weights -= self._dW * lr
         self._biases -= self._db * lr
-
-
-class ReLU(Layer):
-    def __init__(self, output_dim: int) -> None:
-        """Initilize variables."""
-
-        self.units: int = output_dim
-
-    def forward(self, x: T.Tensor) -> T.Tensor:
-        """Perform a forward pass."""
-
-        self._prev: T.Tensor = T.Tensor(np.maximum(0, x.data))
-        return self._prev
-
-    def backward(self, dF: T.Tensor) -> None:
-        """Perform a backpropagation.
-
-        There are two cases:
-           (1) if x <= 0, derivative is 0' = 0
-           (2) if x >  0, derivative is x' = 1
-        """
-
-        return dF * T.Tensor(np.where(self._prev <= 0, 0, 1))
-
-
-class Sigmoid(Layer):
-    def __init__(self, output_dim: int) -> None:
-        """Initilize variables."""
-
-        self._units: int = output_dim
-
-    def forward(self, x: T.Tensor) -> T.Tensor:
-        """Perform a forward pass."""
-
-        self._prev: T.Tensor = T.Tensor(1 / (1 + np.exp(-x.data)))
-        return self._prev
-
-    def backward(self, dF: T.Tensor) -> T.Tensor:
-        """Perform a backpropagation.
-
-        If the sigmoid function is denoted as s, it can be shown that the
-        derivative is computed as s * (1 - s).
-        """
-
-        return dF * self._prev * (1 - self._prev)
-
-
-class Tanh(Layer):
-    def __init__(self, output_dim: int) -> None:
-        """Initilize variables."""
-
-        self._units: int = output_dim
-
-    def forward(self, x: T.Tensor) -> T.Tensor:
-        """Perform a forward pass."""
-
-        self._prev: T.Tensor = T.Tensor(np.tanh(x.data))
-        return self._prev
-
-    def backward(self, dF: T.Tensor) -> T.Tensor:
-        """Perform a backpropagation.
-
-        tanh(x) = (e^x - e^{-x}) / (e^x + e^{-x})
-
-        tanh'(x) = ((e^x + e^{-x})^2 - (e^x - e^{-x})^2) / (e^x + e^{-x})^2
-                 = 1 - ((e^x - e^{-x}) / (e^x + e^{-x}))^2
-                 = 1 - tanh^2(x)
-        """
-
-        return T.Tensor(dF * (1 - np.square(np.tanh(self._prev.data))))
-
-
-class Softmax(Layer):
-    def __init__(self, output_dim: int) -> None:
-        """Initilize variables."""
-
-        self._units: int = output_dim
-
-    def forward(self, x: T.Tensor) -> T.Tensor:
-        """Perform a forward pass."""
-
-        exp: np.ndarray = np.exp(x.data - np.max(x.data))
-        elf._prev: Tensor = exp / np.sum(exp)
-        return self._prev
-
-    def backward(self, dF: T.Tensor) -> T.Tensor:
-        """Perform a backpropagation."""
-
-        exp: np.ndarray = np.exp(self._prev.data - np.max(self._prev.data))
-        val: np.ndarray = exp / np.sum(exp)
-        return T.Tensor(-np.outer(val, val) + np.diag(val.flatten()))
--- miniml/loss.py
+++ miniml/loss.py
@@ -5,15 +5,13 @@ import miniml.tensor as T
 # TODO: Consider using an abstract class for loss functions as well
 
 
-class MSE:
-    """The means squared error loss function."""
-
+class MeanSquaredError:
     def __init__(self, pred: T.Tensor, real: T.Tensor):
-        self._pred: T.Tensor = pred
-        self._real: T.Tensor = real
+        self._pred = pred
+        self._real = real
 
     def forward(self):
-        return T.Tensor(np.power((self._pred - self._real).data, 2).mean())
+        return T.Ops.mean(T.Ops.power(self._pred - self._real, 2))
 
     def backward(self):
-        return 2 * ((self._pre - self._real).data).mean()
+        return 2 * T.Ops.mean(self._pred - self._real)
